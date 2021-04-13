20a81c0299e5095e9a71f805496247129c10c666
Luluk Mufida <lulukmufida0015@gmail.com>
2021-04-13 19:05:15 +0700

    Praktikum 2 selesai

--- app/Http/Controllers/ArticleController.php
+++ app/Http/Controllers/ArticleController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\Article;
 use Illuminate\Http\Request;
+use Storage;
 
 class ArticleController extends Controller
 {
--- resources/views/articles/edit.blade.php
+++ resources/views/articles/edit.blade.php
@@ -2,8 +2,8 @@
 
 @section('content')
 <div class="container">
-    <form action="/articles/{{$article->id}}" method="post" 
-    enctype="multipart/form-data">
+    <form action="/articles/{{$article->id}}" method="post" enctype="multipart/form-data">
+        @method('PUT')
         @csrf
         <div class="form-group">
             <label for="title">Judul</label>
